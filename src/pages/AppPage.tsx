a77d0786eb035356cb8d4ae4a1385c9e0929e274
Ali Gheisar <ali.gheisar.dev@gmail.com>
2025-07-23 18:40:21 +0330

    add some new styles for project manager

--- src/pages/AppPage.tsx
+++ src/pages/AppPage.tsx
@@ -1,5 +1,20 @@
+import { projects } from "@/mock/projects";
+
 const AppPage = () => {
-  return <h1>projects</h1>;
+  return (
+    <section className="app-container">
+      <h1>projects</h1>
+      <div className="projects-container">
+        {projects.map((proj) => (
+          <article key={proj.id}>
+            <span>{proj.id}</span>
+            <h3>{proj.name}</h3>
+            <p>{proj.description}</p>
+          </article>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default AppPage;
