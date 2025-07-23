022b8f56e3b5d7ddd4bb58d035c7b736c1bb7681
Ali Gheisar <ali.gheisar.dev@gmail.com>
2025-07-23 14:41:52 +0330

    add timeout to keyBuffer

--- src/commands/keyBuffer.ts
+++ src/commands/keyBuffer.ts
@@ -1,8 +1,16 @@
 let buffer: string[] = [];
+let bufferTimeout: NodeJS.Timeout | null = null;
 
 export const keyBuffer = {
   addKey: (key: string) => {
+    if (bufferTimeout) clearTimeout(bufferTimeout);
+
     buffer.push(key);
+
+    bufferTimeout = setTimeout(() => {
+      buffer = [];
+    }, 500);
+
     return [...buffer];
   },
   clear: () => {
