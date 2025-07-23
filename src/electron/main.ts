dad421e79b2a6cdb0f97bf3215cacd401ecc1bc6
Ali Gheisar <ali.gheisar.dev@gmail.com>
2025-07-23 12:57:04 +0330

    change some settings of project window

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -12,6 +12,8 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    frame: false,
+    title: "vimap",
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
@@ -27,7 +29,7 @@ const createWindow = () => {
   }
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
