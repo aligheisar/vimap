3ed9d6eb76210d09acc9ab5c990b01a0920e2359
Ali Gheisar <ali.gheisar.dev@gmail.com>
2025-07-24 11:54:59 +0330

    add a mock data

--- src/mock/projects.ts
+++ src/mock/projects.ts
@@ -6,7 +6,18 @@ export const projects: Project[] = [
     name: "My Web App",
     description:
       "this is a fake description that im writing for delectation peipus",
-    color: "blue",
+    pinned: true,
+    tags: ["personal", "frontend"],
+    archived: false,
+    createdAt: "2025-07-22T10:01:00Z",
+    updatedAt: "2025-07-22T10:30:00Z",
+  },
+  {
+    id: "proj_002",
+    name: "My New Web App",
+    description:
+      "this is also a fake description that im writing for delectation peipus",
+    color: "#aa0022",
     pinned: true,
     tags: ["personal", "frontend"],
     archived: false,
