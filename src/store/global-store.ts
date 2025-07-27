031e4f3197db5e375f35a46c2b55f4b53a323552
Ali Gheisar <ali.gheisar.dev@gmail.com>
2025-07-27 09:06:48 +0330

    remove user from global store

--- src/store/global-store.ts
+++ src/store/global-store.ts
@@ -5,7 +5,6 @@ import type { Mode } from "@/types/mode";
 type State = {
   focusedPane: CommandContext;
   currentMode: Mode;
-  user: any;
 };
 
 type Action = {
@@ -16,7 +15,6 @@ type Action = {
 export const useGlobalStore = create<State & Action>((set) => ({
   focusedPane: "global",
   currentMode: "normal",
-  user: null,
   setFocusedPane: (focusedPane) => set({ focusedPane }),
   setCurrentMode: (currentMode) => set({ currentMode }),
 }));
